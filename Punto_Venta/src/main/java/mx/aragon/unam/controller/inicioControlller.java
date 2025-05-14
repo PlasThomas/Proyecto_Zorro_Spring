71f99b5258bc47a9ce933713900f74b877388657
PlasThomas <joseplascencia346@aragon.unam.mx>
2025-05-13 18:22:55 -0600

    Correccion de errores

--- Punto_Venta/src/main/java/mx/aragon/unam/controller/inicioControlller.java
+++ Punto_Venta/src/main/java/mx/aragon/unam/controller/inicioControlller.java
@@ -1,13 +1,12 @@
 package mx.aragon.unam.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class inicioControlller {
-    @GetMapping("/")
+    @RequestMapping(value = "/",method = RequestMethod.GET)
     public String inicio(){
         return "vistas/index";
     }
